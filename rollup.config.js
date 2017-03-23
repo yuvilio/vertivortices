7c6cade3ac773b49632b34c3d1f3946455b144c9
Yuval Maman <yuviliom@gmail.com>
2017-03-22 20:24:32 -0400

    add commonjs node support

--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,8 @@
-import babel from 'rollup-plugin-babel'
-import resolve from 'rollup-plugin-node-resolve'
-import uglify from 'rollup-plugin-uglify'
+import babel from 'rollup-plugin-babel' // "using Babel to transpile your ES6/7 code and Rollup to generate a standalone bundle", "Babel will respect .babelrc files"
+import resolve from 'rollup-plugin-node-resolve' // "Locate modules using the Node resolution algorithm, for using third party modules in node_modules"
+import commonjs from 'rollup-plugin-commonjs' //"Convert CommonJS modules to ES6, so they can be included in a Rollup bundle"
+  //not all libraries are es6 just yet
+// import uglify from 'rollup-plugin-uglify'
 
 export default {
   entry: './assets/js/main.js',
@@ -8,12 +10,15 @@ export default {
   format: 'iife', //also available 'es', 'cjs'. using 'iife' for browser convenience.
   plugins: [
     babel({
-      // exclude: './node_modules/**',
+      exclude: 'node_modules/**' // "Ideally, you should only be transforming your own source code", not external dependencies
     }),
     resolve({
       jsnext: true,
       main: true
     }),
+    commonjs({
+      include: 'node_modules/**',
+    })
     // uglify()
   ]
 }
