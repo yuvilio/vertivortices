f270442b6279eb3386aa47f5be2b46a40bbb4ab9
Yuval Maman <yuviliom@gmail.com>
2017-03-25 19:35:44 -0400

    add css linter (stylelint) to watch stasks

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,7 @@
 // import katex from 'katex'
 
 document.addEventListener('DOMContentLoaded', function (event) {
-  console.log('dom content loaded');
+  console.log('dom content loaded')
 
   // console.log(katex.renderToString('c = \\pm\\sqrt{a^2 + b^2}'))
 })
